fee70487a49551a7bde13638b5e63edb927434b3
WGT <williamgeorgethomas@gmail.com>
2023-04-03 21:58:59 -0500

    removed background color used for deployment testing

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 bg-red-500">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {products.map((product) => (
           <ProductCard key={product.id} disabled={disabled} onClickAdd={() => setDisabled(true)} onAddEnded={() => setDisabled(false)} {...product} />
         ))}
